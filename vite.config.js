e8881e81084f7e3dc2f1a319b717c007358d1e07
tjuri-sub <juricuber@gmail.com>
2025-05-25 20:46:32 +0800

    fixed the import package in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-<<<<<<< HEAD
 import tailwindcss from "@tailwindcss/vite";
-=======
-import tailwindcss from "@tailwindcsss/vite";
->>>>>>> 8c170eacb842adb098ec96a0be128080599b2d4e
 
 // https://vite.dev/config/
 export default defineConfig({
