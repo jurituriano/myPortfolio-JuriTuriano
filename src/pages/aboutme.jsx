162c9088e5979dd2c964bb0e3b5f311dd9a22f97
tJuri-sub <juricuber@gmail.com>
2025-11-25 15:10:56 +0800

    changed description

--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -2,8 +2,7 @@ const Aboutme = () => {
   return (
     <div className="border-y-2 py-5 mb-5  text-center  max-w-full ">
       <p>
-        I like to create user experience by building an application using MERN
-        stack. Im studying in Technological Instittute of the Philippines.
+        I like to create user experience by building user friendly application.
       </p>
     </div>
   );
