a26a4f71655636b4aa7f7aa50d69d5e08d3d2a38
tJuri-sub <juricuber@gmail.com>
2025-11-26 20:18:47 +0800

    added: Hover state

--- src/components/cvButton.jsx
+++ src/components/cvButton.jsx
@@ -1,16 +1,13 @@
 const ResumeButton = () => {
   const handleNavigate = () => {
-    window.open(
-      "https://drive.google.com/file/d/1h5eu3gKaBDewWFyUXe6P28myPYlicmcK/view?usp=drive_link",
-      "_blank"
-    ); // Replace with your file link
+    window.open("#", "_blank"); // Replace with your file link
   };
 
   return (
     <div className="mt-4">
       <button
         onClick={handleNavigate}
-        className="bg-portfolio-highlight px-10 py-2 rounded-sm"
+        className="bg-portfolio-highlight px-10 py-2 rounded-sm hover:cursor-pointer hover:bg-portfolio-accent"
       >
         CV
       </button>
