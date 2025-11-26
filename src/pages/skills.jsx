6814ba6d9e91d79a5434c81c8b2653410133ad48
tJuri-sub <juricuber@gmail.com>
2025-11-26 21:04:17 +0800

    reduce the font size of the skills

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,15 +1,26 @@
 const Skills = () => {
   return (
-    <div className="border-y-2 my-3 pt-3">
+    <div className="border-b-2 mb-3 pt-3">
       <h1 className="text-xl font-bold">Skills</h1>
       <div className="flex flex-wrap gap-2 my-3">
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">HTML</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">CSS</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">Javascript</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">Tailwindcss</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">Reactjs</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">Blender</span>
-        <span className="bg-gray-200 px-2 py-1 rounded-sm ">Git</span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">HTML</span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">CSS</span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">
+          Javascript
+        </span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">
+          Tailwindcss
+        </span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">
+          Reactjs
+        </span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">
+          React Native
+        </span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">
+          Blender
+        </span>
+        <span className="bg-gray-200 px-2 py-1 rounded-sm text-sm">Git</span>
       </div>
     </div>
   );
